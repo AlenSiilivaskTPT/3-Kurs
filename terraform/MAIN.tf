bfb5f8e97807df4b821b6bf1b1f4c58eb04dd368
TheAlestGuy <46711734+TheAlestGuy@users.noreply.github.com>
2020-03-13 11:13:50 +0200

    Update MAIN.tf

--- terraform/MAIN.tf
+++ terraform/MAIN.tf
@@ -1,1 +1,27 @@
-{\rtf1}
+provider "azurerm" {
+  subscription_id = "${var.subscription}"
+  version = "v1.44.0"
+}
+
+resource "azurerm_resource_group" "myResource" {
+  name     = "${var.rgName}"
+  location = "${var.region}"
+}
+
+resource "azurerm_storage_account" "tpt" {
+  name                     = "dpsfgjsdg"
+  resource_group_name      = "${azurerm_resource_group.myResource.name}"
+  location                 = "${azurerm_resource_group.myResource.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tpt" {
+  name                  = "dpsfgjsdg"
+  storage_account_name  = "${azurerm_storage_account.tpt.name}"
+  container_access_type = "private"
+}
