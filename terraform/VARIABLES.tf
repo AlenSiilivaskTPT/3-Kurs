cbbee171bdf885d463da2d8b80236e0b28ba4b1b
TheAlestGuy <46711734+TheAlestGuy@users.noreply.github.com>
2020-03-13 11:14:11 +0200

    Update VARIABLES.tf

--- terraform/VARIABLES.tf
+++ terraform/VARIABLES.tf
@@ -1,1 +1,12 @@
-{\rtf1}
+variable "subscription" {
+  default = "917fd69d-d9dd-4861-865e-88fd03c63b5b"
+  description = "default id"
+}
+
+variable "rgName" {
+  default = "sampLeRG"
+}
+
+variable "region" {
+  default = "West Europe"
+}
